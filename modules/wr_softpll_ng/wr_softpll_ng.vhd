836bec09f74b2196cf41a21b9a6b188d9e431d9a
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2023-04-03 15:41:57 +0200

    xwrc_board_[common|spec] connect all this fractional-N softpll stuff through the numerous layers of wrappers... :/

--- modules/wr_softpll_ng/wr_softpll_ng.vhd
+++ modules/wr_softpll_ng/wr_softpll_ng.vhd
@@ -6,7 +6,7 @@
 -- Author     : Tomasz Włostowski
 -- Company    : CERN BE-CO-HT
 -- Created    : 2011-01-29
--- Last update: 2022-01-28
+-- Last update: 2023-04-03
 -- Platform   : FPGA-generic
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -113,14 +113,14 @@ entity wr_softpll_ng is
 
 -- DMTD Offset clock
     clk_dmtd_i : in std_logic;
-    clk_dmtd_over_i : in std_logic;
+    clk_dmtd_over_i : in std_logic := '0';
 
 -- External reference clock (e.g. 10 MHz from Cesium/GPSDO). Used only if
 -- g_num_exts > 0
-    clk_ext_i : in std_logic;
+    clk_ext_i : in std_logic := '0';
 
 -- External clock, multiplied to 125 MHz using the FPGA's PLL
-    clk_ext_mul_i        : in std_logic_vector(f_nonzero_vector(g_num_exts)-1 downto 0);
+    clk_ext_mul_i        : in std_logic_vector(f_nonzero_vector(g_num_exts)-1 downto 0) := (others => '0');
     clk_ext_mul_locked_i : in std_logic := '1';
     clk_ext_stopped_i : in std_logic := '0';
     clk_ext_rst_o : out std_logic;
@@ -451,7 +451,7 @@ begin  -- rtl
   end generate gen_feedback_dmtds;
 
   -- drive unused debug output
-  debug_o(4) <= '0';
+--  debug_o(4) <= '0';
 
   gen_ext_dmtds: for I in 0 to g_num_exts-1 generate
 
@@ -487,9 +487,9 @@ begin  -- rtl
   end generate gen_ext_dmtds;
 
   gen_with_ext_clock_input: if g_num_exts > 0 generate
-    debug_o(0) <= fb_resync_out(0);
-    debug_o(1) <= tags_p(g_num_ref_inputs + g_num_outputs);
-    debug_o(2) <= tags_p(g_num_ref_inputs);
+--    debug_o(0) <= fb_resync_out(0);
+--    debug_o(1) <= tags_p(g_num_ref_inputs + g_num_outputs);
+--    debug_o(2) <= tags_p(g_num_ref_inputs);
     
     U_Aligner_EXT : spll_aligner
       generic map (
@@ -534,11 +534,11 @@ begin  -- rtl
     regs_out.eccr_ext_ref_stopped_i          <= '0';
     clk_ext_rst_o <= '0';
     -- drive unused debug outputs
-    debug_o(0) <= '0';
-    debug_o(1) <= '0';
-    debug_o(2) <= '0';
-    debug_o(3) <= '0';
-    debug_o(5) <= '0';
+--    debug_o(0) <= '0';
+--    debug_o(1) <= '0';
+--    debug_o(2) <= '0';
+--    debug_o(3) <= '0';
+--    debug_o(5) <= '0';
   end generate gen_without_ext_clock_input;
 
   p_ack_aligner_samples: process(regs_in, aligner_sample_valid)
