530517d431ef1b2cacfbeb8ffd063981886f0e7f
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2023-07-17 00:15:24 +0200

    wr_core: added optional dithered DAC driver module

--- modules/wrc_core/Manifest.py
+++ modules/wrc_core/Manifest.py
@@ -7,5 +7,6 @@ files = [ "xwr_core.vhd",
           "wrc_diags_dpram.vhd",
 	  "wrc_urv_wrapper.vhd",
 	  "wrc_cpu_csr_wbgen2_pkg.vhd",
-	  "wrc_cpu_csr_wb.vhd"
+	  "wrc_cpu_csr_wb.vhd",
+          "wrc_dac_driver.vhd"
 ];
