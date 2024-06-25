7ef1e052f0b8851f11b24f2360f616d31f3eb9f0
Imre Pechan <Imre.Pechan@extern.sckcen.be>
2024-06-25 15:22:45 +0200

    Update tcl to Vivado2022.2; fix git module refs in top manifest

--- top/damc-fmc2zup_ref_design/Manifest.py
+++ top/damc-fmc2zup_ref_design/Manifest.py
@@ -12,7 +12,8 @@ modules = {
         "../../board/damc-fmc2zup"
     ],
     "git" : [
-        "git://ohwr.org/hdl-core-lib/general-cores.git",
-        "git://ohwr.org/hdl-core-lib/etherbone-core.git",
+        "git://ohwr.org/project/general-cores.git",
+        "git://ohwr.org/project/etherbone-core.git",
+        "git://ohwr.org/project/urv-core.git",
     ],
 }
