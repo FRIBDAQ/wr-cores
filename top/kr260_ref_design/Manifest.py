0b5eeac10dfe20d6aab2d58822898d374ea97ca7
Tristan Gingold <tristan.gingold@cern.ch>
2024-07-29 13:14:40 +0200

    Initial files for kr260
    blinking leds

---
+++ top/kr260_ref_design/Manifest.py
@@ -0,0 +1,12 @@
+fetchto = "../../ip_cores"
+
+files = [ "kr260_ref_top.vhd", ]
+
+modules = {
+    "local" : [
+        "../../",
+    ],
+    "git" : [
+        "git://ohwr.org/hdl-core-lib/general-cores.git",
+    ],
+}
