a54ebba69c2f5ca6884130a279bd1c8b722d3026
Cedric Viou <cedric.viou@obs-nancay.fr>
2024-08-21 13:43:18 +0200

    Fix idrogen integration

--- board/Manifest.py
+++ board/Manifest.py
@@ -1,5 +1,5 @@
 try:
-    if board in ["spec", "svec", "vfchd", "clbv2", "clbv3", "clbv4", "pxie-fmc", "diot-sb", "common"]:
+    if board in ["spec", "svec", "vfchd", "clbv2", "clbv3", "clbv4", "pxie-fmc", "diot-sb", "idrogen_v3", "common"]:
         modules = {"local" : [ board ] }
 except NameError:
     pass
