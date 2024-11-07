cae9caf6a1c5e7e470d9885b03f1e566555386df
Harvey Leicester <harvey.macdonald.leicester@cern.ch>
2024-11-07 13:44:33 +0100

    adding serdes for spartan6 and kintex7

---
+++ platform/xilinx/wrc_serdes/Manifest.py
@@ -0,0 +1,4 @@
+if (syn_device[0:4].upper()=="XC6S"):    # Spartan6
+	files = ["oserdes_8_to_1_spartan6.vhd"]
+elif (syn_device[0:4].upper()=="XC7K" or syn_device[0:4].upper()=="XC7A"):    # Kintex7, Artix7
+  files = ["oserdes_8_to_1_7series.vhd"]
