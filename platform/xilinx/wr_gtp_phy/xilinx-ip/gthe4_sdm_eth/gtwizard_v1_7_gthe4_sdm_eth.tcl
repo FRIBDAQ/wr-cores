c301fd56c57a81f8790c03fe25f57b1dca488ce5
Oskar Szakinnis <oskar.szakinnis@missinglinkelectronics.com>
2024-12-10 17:48:36 +0100

    platform/xilinx/wr_gtp_phy/xilinx-ip/gthe4_sdm_eth/gtwizard_v1_7_gthe4_sdm_eth.tcl: fix IP generation, update location to SFP2 (matching ZCU102 board constraints)

--- platform/xilinx/wr_gtp_phy/xilinx-ip/gthe4_sdm_eth/gtwizard_v1_7_gthe4_sdm_eth.tcl
+++ platform/xilinx/wr_gtp_phy/xilinx-ip/gthe4_sdm_eth/gtwizard_v1_7_gthe4_sdm_eth.tcl
@@ -4,9 +4,9 @@ create_ip -name gtwizard_ultrascale -vendor xilinx.com -library ip -version 1.7 
 
 set_property CONFIG.preset {GTH-Gigabit_Ethernet} [get_ips "${module_name}"]
 set_property -dict [list \
-  CONFIG.CHANNEL_ENABLE {X1Y12} \
-  CONFIG.RX_MASTER_CHANNEL {X1Y12} \
-  CONFIG.TX_MASTER_CHANNEL {X1Y12} \
+  CONFIG.CHANNEL_ENABLE {X1Y14} \
+  CONFIG.RX_MASTER_CHANNEL {X1Y14} \
+  CONFIG.TX_MASTER_CHANNEL {X1Y14} \
   CONFIG.TX_BUFFER_MODE {0} \
   CONFIG.RX_BUFFER_MODE {0} \
   CONFIG.RX_SLIDE_MODE {PCS} \
@@ -14,6 +14,15 @@ set_property -dict [list \
   CONFIG.LOCATE_IN_SYSTEM_IBERT_CORE {EXAMPLE_DESIGN} \
   CONFIG.LOCATE_TX_USER_CLOCKING {CORE} \
   CONFIG.LOCATE_RX_USER_CLOCKING {CORE} \
+  CONFIG.RX_PLL_TYPE {QPLL1} \
+  CONFIG.RX_QPLL_FRACN_NUMERATOR {261990} \
+  CONFIG.RX_REFCLK_FREQUENCY {124.975605} \
+  CONFIG.RX_REFCLK_SOURCE {X1Y14 clk1} \
+  CONFIG.TXPROGDIV_FREQ_ENABLE {true} \
+  CONFIG.TX_PLL_TYPE {QPLL0} \
+  CONFIG.TX_QPLL_FRACN_NUMERATOR {261990} \
+  CONFIG.TX_REFCLK_FREQUENCY {124.975605} \
+
 ] [get_ips "${module_name}"]
 
 generate_target all [get_ips "${module_name}"]
