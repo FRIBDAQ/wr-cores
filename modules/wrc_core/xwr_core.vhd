1637d2105fbe843e92217cd5091a465a15e554ec
Tristan Gingold <tristan.gingold@cern.ch>
2025-01-09 13:50:40 +0100

    xwr_core.vhd: fix memory map comment

--- modules/wrc_core/xwr_core.vhd
+++ modules/wrc_core/xwr_core.vhd
@@ -42,17 +42,18 @@
 --
 -------------------------------------------------------------------------------
 -- Memory map:
---  0x00000000: I/D Memory
---  0x00020000: Peripheral interconnect
---      +0x000: Minic
---      +0x100: Endpoint
---      +0x200: Softpll
---      +0x300: PPS gen
---      +0x400: Syscon
---      +0x500: UART
---      +0x600: OneWire
---      +0x700: Auxillary space (Etherbone config, etc)
---      +0x800: WRPC diagnostics registers
+--      0x000: Minic
+--      0x100: Endpoint
+--      0x200: Softpll
+--      0x300: PPS gen
+--      0x400: Syscon  (periph 0)
+--      0x500: UART    (periph 1)
+--      0x600: OneWire (periph 2)
+--      0x800: WRPC diagnostics registers (for user)     (periph 3)
+--      0x900: WRPC diagnostics registers (for firmware) (periph 4)
+--      0xa00: freq monitor
+--      0xb00: cpu csr
+--      0x8000: Auxillary space (Etherbone config, etc)
 
 library ieee;
 use ieee.std_logic_1164.all;
