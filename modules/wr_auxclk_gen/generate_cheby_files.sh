065850f584e7b16650419bd3e342a8864aa579e8
Harvey Leicester <harvey.macdonald.leicester@cern.ch>
2025-01-13 14:40:35 +0100

    auxclk_gen: update cheby header name

--- modules/wr_auxclk_gen/generate_cheby_files.sh
+++ modules/wr_auxclk_gen/generate_cheby_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cheby -i auxclk_regs.cheby --gen-hdl auxclk_regs.vhd
-cheby -i auxclk_regs.cheby --gen-c auxclk_regs.h
+cheby -i auxclk_regs.cheby --gen-c auxclk_gen.h
 cheby -i auxclk_regs.cheby --gen-consts ../../sim/regs/auxclk_regs.sv
