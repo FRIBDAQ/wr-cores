74b9189b5198ee4251332bb477d016e261492ad7
Tristan Gingold <tristan.gingold@cern.ch>
2025-06-10 16:58:32 +0200

    kria: directly instantiate gthe4_common

--- top/kr260_ref_design/Manifest.py
+++ top/kr260_ref_design/Manifest.py
@@ -3,8 +3,6 @@ fetchto = "../../ip_cores"
 files = [ "kr260_ref_top.vhd", "wr_gthe4_adapter.vhd",
           "mpsoc.bd", "mpsoc_map.vhd",
           "drp_core.vhd", "drp_map.vhd",
-          "gthe4_common/gthe4_sdm_gthe4_common_wrapper.v",
-          "gthe4_common/gtwizard_ultrascale_v1_7_gthe4_common.v",
         ]
 
 modules = {
