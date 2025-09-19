c26ee4d98fef8daaa666540243fec519a54635a2
Tristan Gingold <tristan.gingold@cern.ch>
2025-09-19 09:37:28 +0200

    rxpi: move files

--- board/gthe4-rxpi/Manifest.py
+++ board/gthe4-rxpi/Manifest.py
@@ -1,6 +1,3 @@
-files = [ "wr_gthe4_adapter.vhd", "wr_gthe4_rxtx_adapter.vhd",
-          "rxpi_gthe4_map.vhd",
-          "gthe4_sdm.tcl",
-          "xwrc_gthe4_rxpi.vhd",
+files = [ "gthe4_sdm.tcl",
           "xwrc_board_gthe4_rxpi.vhd",
         ]
