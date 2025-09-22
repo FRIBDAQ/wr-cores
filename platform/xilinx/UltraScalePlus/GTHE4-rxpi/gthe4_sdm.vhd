9079c3bdf03ba85f12ea546f1b6d8ea334452183
Tristan Gingold <tristan.gingold@cern.ch>
2025-09-22 08:57:17 +0200

    platform/xilinx/family7-gthe4: add gthe4_sdm.vhd

---
+++ platform/xilinx/UltraScalePlus/GTHE4-rxpi/gthe4_sdm.vhd
@@ -0,0 +1,88 @@
+-------------------------------------------------------------------------------
+-- Title      : WRPC reference design for KR260 board
+-- Project    : WR PTP Core
+-- URL        : http://www.ohwr.org/projects/wr-cores/wiki/Wrpc_core
+-------------------------------------------------------------------------------
+-- Company    : CERN (BE-CO-HT)
+-- Standard   : VHDL'93
+-------------------------------------------------------------------------------
+-- Copyright (c) 2024 CERN
+-------------------------------------------------------------------------------
+-- GNU LESSER GENERAL PUBLIC LICENSE
+--
+-- This source file is free software; you can redistribute it
+-- and/or modify it under the terms of the GNU Lesser General
+-- Public License as published by the Free Software Foundation;
+-- either version 2.1 of the License, or (at your option) any
+-- later version.
+--
+-- This source is distributed in the hope that it will be
+-- useful, but WITHOUT ANY WARRANTY; without even the implied
+-- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+-- PURPOSE.  See the GNU Lesser General Public License for more
+-- details.
+--
+-- You should have received a copy of the GNU Lesser General
+-- Public License along with this source; if not, download it
+-- from http://www.gnu.org/licenses/lgpl-2.1.html
+--
+-------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.wishbone_pkg.all;
+
+library unisim;
+use unisim.vcomponents.all;
+
+entity gthe4_sdm is
+  port (
+    --  System clock (same as WR core clk_sys)
+    clk_62m5_i  : in std_logic;
+    rst_n_i     : in std_logic;
+
+    --  WR dac input
+    dac_data_i : in  std_logic_vector(23 downto 0);
+    dac_load_i : in  std_logic;
+
+    --  sdm output
+    sdm_data_o   : out std_logic_vector(24 downto 0);
+    sdm_toggle_o : out std_logic
+  );
+end;
+
+architecture top of gthe4_sdm is
+  signal sdm_cnt : unsigned(5 downto 0);
+begin
+  process(clk_62m5_i)
+  begin
+    if rising_edge(clk_62m5_i) then
+      if rst_n_i = '0' then
+        sdm_cnt <= (others => '0');
+        sdm_toggle_o <= '0';
+        sdm_data_o <= (others => '0');
+      else
+        if sdm_cnt = 0 then
+          --  Idle, can accept a new value
+          if dac_load_i = '1' then
+            --  Reformat.
+            --  According to 73205, only LSB are significant.
+            sdm_data_o <= (others => '0');
+            sdm_data_o(23 downto 0) <= dac_data_i;
+            sdm_cnt <= (others => '1');
+          end if;
+        else
+          --  FB CLK should be way higher than system clock
+          if sdm_cnt(5 downto 4) = "00" then
+            sdm_toggle_o <= '0';
+          elsif sdm_cnt(5 downto 4) /= "11" then
+            sdm_toggle_o <= '1';
+          end if;
+          sdm_cnt <= sdm_cnt - 1;
+        end if;
+      end if;
+    end if;
+  end process;
+end top;
