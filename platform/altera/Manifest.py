dbd1fdc555543573c38e26bdc51aa9a5e8ec0e3b
A. Hahn <a.hahn@gsi.de>
2025-10-08 16:11:31 +0200

    Altera: add wr_arria10_phy dir

--- platform/altera/Manifest.py
+++ platform/altera/Manifest.py
@@ -1,7 +1,8 @@
 def __helper():
   dirs = []
-  if syn_device[:1] == "5":    dirs.extend(["wr_arria5_phy"])
-  if syn_device[:4] == "ep2a": dirs.extend(["wr_arria2_phy"])
+  if syn_device[:3] == "10a":     dirs.extend(["wr_arria10_phy"])
+  if syn_device[:1] == "5":       dirs.extend(["wr_arria5_phy"])
+  if syn_device[:4] == "ep2a":    dirs.extend(["wr_arria2_phy"])
   return dirs
 
 files = [ "wr_altera_pkg.vhd", "xwrc_platform_altera.vhd" ]
