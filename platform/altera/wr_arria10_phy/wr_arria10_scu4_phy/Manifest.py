cdb70b539c1f06b2796e7dbbaf651197ff261981
A. Hahn <a.hahn@gsi.de>
2025-10-08 16:41:50 +0200

    Altera: add arria10 scu4 phy and clocks

---
+++ platform/altera/wr_arria10_phy/wr_arria10_scu4_phy/Manifest.py
@@ -0,0 +1,7 @@
+files = [
+  "wr_arria10_scu4_atx_pll/wr_arria10_scu4_atx_pll.qsys",
+  "wr_arria10_scu4_cmu_pll/wr_arria10_scu4_cmu_pll.qsys",
+  "wr_arria10_scu4_det_phy/wr_arria10_scu4_det_phy.qsys",
+  "wr_arria10_scu4_phy/wr_arria10_scu4_phy.qsys",
+  "wr_arria10_scu4_rst_ctl/wr_arria10_scu4_rst_ctl.qsys"
+  ]
