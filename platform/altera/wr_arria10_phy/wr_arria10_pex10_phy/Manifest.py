eca41b0cfd2a6d20fef7885f0e54ee72b52a38a8
A. Hahn <a.hahn@gsi.de>
2025-10-08 16:41:39 +0200

    Altera: add arria10 pex10 phy and clocks

---
+++ platform/altera/wr_arria10_phy/wr_arria10_pex10_phy/Manifest.py
@@ -0,0 +1,7 @@
+files = [
+  "wr_arria10_pex10_atx_pll/wr_arria10_pex10_atx_pll.qsys",
+  "wr_arria10_pex10_cmu_pll/wr_arria10_pex10_cmu_pll.qsys",
+  "wr_arria10_pex10_det_phy/wr_arria10_pex10_det_phy.qsys",
+  "wr_arria10_pex10_phy/wr_arria10_pex10_phy.qsys",
+  "wr_arria10_pex10_rst_ctl/wr_arria10_pex10_rst_ctl.qsys"
+  ]
