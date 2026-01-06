014f08f4f723699660a5a25fc187a67d0d31a990
Tristan Gingold <tristan.gingold@cern.ch>
2026-01-06 10:29:30 +0100

    Separate CPU from non-cpu part
    Extract xwrc_uncore from xwrc_core

--- modules/wrc_core/Manifest.py
+++ modules/wrc_core/Manifest.py
@@ -4,6 +4,7 @@
 ## SPDX-License-Identifier: LGPL-2.1-or-later
 ###############################################################################
 files = [ "xwr_core.vhd",
+          "xwr_subsystem.vhd",
           "wr_core.vhd",
           "wrcore_pkg.vhd",
           "wrc_periph.vhd",
