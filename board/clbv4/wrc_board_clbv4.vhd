ec87586a4c2192b04c48fa49cd98c1b209a4080f
Peter Jansweijer <peterj@nikhef.nl>
2026-01-09 13:58:39 +0100

    updated licences in files (spdx format) with copyright Nikhef

--- board/clbv4/wrc_board_clbv4.vhd
+++ board/clbv4/wrc_board_clbv4.vhd
@@ -1,4 +1,8 @@
 -------------------------------------------------------------------------------
+-- SPDX-FileCopyrightText: 2019 Nikhef (www.nikhef.nl/en/)
+--
+-- SPDX-License-Identifier: CERN-OHL-W-2.0+
+-------------------------------------------------------------------------------
 -- Title      : WRPC Wrapper for clbv4
 -- Project    : WR PTP Core
 -- URL        : https://gitlab.com/ohwr/project/wr-cores/-/wikis/wrpc-core
@@ -7,34 +11,12 @@
 -- Author(s)  : Pascal Bos <bosp@nikhef.nl>
 -- Company    : Nikhef
 -- Created    : 2019-05-22
--- Last update: 2022-05-18
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
 -- Description: Top-level wrapper for WR PTP core including all the modules
 -- needed to operate the core on the clbv4 board.
 -- Version with no VHDL records on the top-level (mainly for Verilog
 -- instantiation).
--------------------------------------------------------------------------------
--- Copyright (c) 2019 Nikhef
--------------------------------------------------------------------------------
--- GNU LESSER GENERAL PUBLIC LICENSE
---
--- This source file is free software; you can redistribute it   
--- and/or modify it under the terms of the GNU Lesser General   
--- Public License as published by the Free Software Foundation; 
--- either version 2.1 of the License, or (at your option) any   
--- later version.                                               
---
--- This source is distributed in the hope that it will be       
--- useful, but WITHOUT ANY WARRANTY; without even the implied   
--- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR      
--- PURPOSE.  See the GNU Lesser General Public License for more 
--- details.                                                     
---
--- You should have received a copy of the GNU Lesser General    
--- Public License along with this source; if not, download it   
--- from http://www.gnu.org/licenses/lgpl-2.1.html
--- 
 -------------------------------------------------------------------------------
 
 library ieee;
