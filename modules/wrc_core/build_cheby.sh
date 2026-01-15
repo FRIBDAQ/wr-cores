c1887cb2416bde32a44484f346c5750063fb5148
Tristan Gingold <tristan.gingold@cern.ch>
2026-01-15 13:41:08 +0100

    wr_core: cheby-ify wrc_syscon
    Also remove some unused registers (leds).

--- modules/wrc_core/build_cheby.sh
+++ modules/wrc_core/build_cheby.sh
@@ -1,3 +1,4 @@
 cheby -i wrc_cpu_csr.cheby --header=commit --gen-hdl wrc_cpu_csr.vhd --gen-c wrc_cpu_csr.h
 cheby -i wrc_devices_map.cheby --header=commit --gen-hdl wrc_devices_map.vhd
 cheby -i wrc_host_map.cheby --header=commit --gen-hdl wrc_host_map.vhd --gen-c wrc_host_map.h
+cheby -i wrc_syscon_map.cheby --header=commit --gen-hdl wrc_syscon_map.vhd --gen-c wrc_syscon_map.h
--- modules/wrc_core/wrc_syscon.vhd
+++ modules/wrc_core/wrc_syscon.vhd
@@ -21,7 +21,7 @@ use ieee.numeric_std.all;
 library work;
 use work.wrcore_pkg.all;
 use work.wishbone_pkg.all;
-use work.sysc_wbgen2_pkg.all;
+use work.wrc_syscon_map_pkg.all;
 
 entity wrc_syscon is
   generic(
@@ -68,23 +68,22 @@ entity wrc_syscon is
 end wrc_syscon;
 
 architecture struct of wrc_syscon is
-  signal sysc_regs_i : t_sysc_in_registers;
-  signal sysc_regs_o : t_sysc_out_registers;
+  signal sysc_regs_i : t_sysc_regs_master_in;
+  signal sysc_regs_o : t_sysc_regs_master_out;
 
   signal cntr_div      : unsigned(23 downto 0);
   signal cntr_tics     : unsigned(31 downto 0);
   signal cntr_overflow : std_logic;
-  
-  signal diag_adr : unsigned(15 downto 0);
+
   signal diag_dat : std_logic_vector(31 downto 0);
   signal diag_out_regs : t_generic_word_array(g_diag_rw_size - 1 downto 0);
   signal diag_in       : t_generic_word_array(g_diag_ro_size + g_diag_rw_size-1 downto 0);
 
   constant c_RESET_CHAIN_LENGTH : integer := 3;
-  
+
   signal rst_net_n : std_logic;
   signal rst_net_n_chain : std_logic_vector(c_RESET_CHAIN_LENGTH -1 downto 0);
-  
+
 begin
 
   -- async assert, sync de-assert reset.
@@ -98,42 +97,43 @@ begin
   end process;
 
   rst_net_n_o <= rst_net_n_chain(0);
-  
+
   process(clk_sys_i)
   begin
     if rising_edge(clk_sys_i) then
       if(rst_n_i = '0') then
         rst_net_n <= '0';
+      elsif sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpsr_net_rst = '1' then
+        rst_net_n <= '0';
       else
-        rst_net_n <= not sysc_regs_o.gpsr_net_rst_o;
-      end if; 
-    end if; 
+        rst_net_n <= '1';
+      end if;
+    end if;
   end process;
-  
+
   -------------------------------------
   -- buttons
   -------------------------------------
-  sysc_regs_i.gpsr_btn1_i <= btn1_i;
-  sysc_regs_i.gpsr_btn2_i <= btn2_i;
+  sysc_regs_i.gpsr_btn1 <= btn1_i;
+  sysc_regs_i.gpsr_btn2 <= btn2_i;
 
   -------------------------------------
   -- MEMSIZE
   -------------------------------------
-  sysc_regs_i.hwfr_memsize_i(3 downto 0) <= g_memsize;
+  sysc_regs_i.hwfr_memsize <= g_memsize;
 
   -------------------------------------
   -- BOARD NAME and Flash info
   -------------------------------------
-  sysc_regs_i.hwir_name_i         <= g_board_name;
-  sysc_regs_i.hwfr_storage_sec_i  <= std_logic_vector(to_unsigned(g_flash_secsz_kb, 16));
-  sysc_regs_i.hwfr_storage_type_i <= "00";  -- for now these parameters are only for Flash
-  sysc_regs_i.sdbfs_baddr_i       <= std_logic_vector(to_unsigned(g_flash_sdbfs_baddr, 32));
+  sysc_regs_i.hwir_name         <= g_board_name;
+  sysc_regs_i.hwfr_storage_sec  <= std_logic_vector(to_unsigned(g_flash_secsz_kb, 16));
+  sysc_regs_i.hwfr_storage_type <= "00";  -- for now these parameters are only for Flash
+  sysc_regs_i.sdbfs_baddr       <= std_logic_vector(to_unsigned(g_flash_sdbfs_baddr, 32));
 
   -------------------------------------
   -- TIMER
   -------------------------------------
-  sysc_regs_i.tvr_i      <= std_logic_vector(cntr_tics);
-  sysc_regs_i.tcr_tdiv_i <= (others => '0');
+  sysc_regs_i.tvr      <= std_logic_vector(cntr_tics);
 
   process(clk_sys_i)
   begin
@@ -141,7 +141,7 @@ begin
       if(rst_n_i = '0') then
         cntr_div      <= (others => '0');
         cntr_overflow <= '0';
-      elsif(sysc_regs_o.tcr_enable_o = '1') then
+      elsif sysc_regs_o.tcr_enable = '1' then
         if(cntr_div = g_cntr_period-1) then
           cntr_div      <= (others => '0');
           cntr_overflow <= '1';
@@ -175,23 +175,23 @@ begin
         scl_o <= '1';
         sda_o <= '1';
       else
-        if(sysc_regs_o.gpsr_fmc_sda_load_o = '1' and sysc_regs_o.gpsr_fmc_sda_o = '1') then
+        if sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpsr_fmc_sda = '1' then
           sda_o <= '1';
-        elsif(sysc_regs_o.gpcr_fmc_sda_o = '1') then
+        elsif sysc_regs_o.gpcr_wr = '1' and sysc_regs_o.gpcr_fmc_sda = '1' then
           sda_o <= '0';
         end if;
 
-        if(sysc_regs_o.gpsr_fmc_scl_load_o = '1' and sysc_regs_o.gpsr_fmc_scl_o = '1') then
+        if sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpsr_fmc_scl = '1' then
           scl_o <= '1';
-        elsif(sysc_regs_o.gpcr_fmc_scl_o = '1') then
+        elsif sysc_regs_o.gpcr_wr = '1' and sysc_regs_o.gpcr_fmc_scl = '1' then
           scl_o <= '0';
         end if;
       end if;
     end if;
   end process;
 
-  sysc_regs_i.gpsr_fmc_sda_i <= sda_i;
-  sysc_regs_i.gpsr_fmc_scl_i <= scl_i;
+  sysc_regs_i.gpsr_fmc_sda <= sda_i;
+  sysc_regs_i.gpsr_fmc_scl <= scl_i;
 
   -------------------------------------
   -- I2C - SFP
@@ -203,25 +203,25 @@ begin
         sfp_scl_o <= '1';
         sfp_sda_o <= '1';
       else
-        if(sysc_regs_o.gpsr_sfp1_sda_load_o = '1' and sysc_regs_o.gpsr_sfp1_sda_o = '1') then
+        if sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpsr_sfp1_sda = '1' then
           sfp_sda_o <= '1';
-        elsif(sysc_regs_o.gpcr_sfp1_sda_o = '1') then
+        elsif sysc_regs_o.gpcr_wr = '1' and sysc_regs_o.gpcr_sfp1_sda = '1' then
           sfp_sda_o <= '0';
         end if;
 
-        if(sysc_regs_o.gpsr_sfp1_scl_load_o = '1' and sysc_regs_o.gpsr_sfp1_scl_o = '1') then
+        if sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpsr_sfp1_scl = '1' then
           sfp_scl_o <= '1';
-        elsif(sysc_regs_o.gpcr_sfp1_scl_o = '1') then
+        elsif sysc_regs_o.gpcr_wr = '1' and sysc_regs_o.gpcr_sfp1_scl = '1' then
           sfp_scl_o <= '0';
         end if;
       end if;
     end if;
   end process;
 
-  sysc_regs_i.gpsr_sfp1_sda_i <= sfp_sda_i;
-  sysc_regs_i.gpsr_sfp1_scl_i <= sfp_scl_i;
+  sysc_regs_i.gpsr_sfp1_sda <= sfp_sda_i;
+  sysc_regs_i.gpsr_sfp1_scl <= sfp_scl_i;
 
-  sysc_regs_i.gpsr_sfp1_det_i <= sfp_det_i;
+  sysc_regs_i.gpsr_sfp1_det <= sfp_det_i;
 
   -------------------------------------
   -- SPI - Flash
@@ -234,42 +234,42 @@ begin
         spi_mosi_o  <= '0';
         spi_ncs_o   <= '1';
       else
-        if(sysc_regs_o.gpsr_spi_sclk_load_o = '1' and sysc_regs_o.gpsr_spi_sclk_o = '1') then
+        if sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpsr_spi_sclk = '1' then
           spi_sclk_o <= '1';
-        elsif(sysc_regs_o.gpcr_spi_sclk_o = '1') then
+        elsif sysc_regs_o.gpcr_wr = '1' and sysc_regs_o.gpcr_spi_sclk = '1' then
           spi_sclk_o <= '0';
         end if;
 
-        if(sysc_regs_o.gpsr_spi_ncs_load_o = '1' and sysc_regs_o.gpsr_spi_ncs_o = '1') then
+        if sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpsr_spi_ncs = '1' then
           spi_ncs_o <= '1';
-        elsif(sysc_regs_o.gpcr_spi_cs_o = '1') then
+        elsif sysc_regs_o.gpcr_wr = '1' and sysc_regs_o.gpcr_spi_cs = '1' then
           spi_ncs_o <= '0';
         end if;
 
-        if(sysc_regs_o.gpsr_spi_mosi_load_o = '1' and sysc_regs_o.gpsr_spi_mosi_o = '1') then
+        if sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpsr_spi_mosi = '1' then
           spi_mosi_o <= '1';
-        elsif(sysc_regs_o.gpcr_spi_mosi_o = '1') then
+        elsif sysc_regs_o.gpsr_wr = '1' and sysc_regs_o.gpcr_spi_mosi = '1' then
           spi_mosi_o <= '0';
         end if;
       end if;
     end if;
   end process;
-  sysc_regs_i.gpsr_spi_sclk_i <= '0';
-  sysc_regs_i.gpsr_spi_ncs_i  <= '0';
-  sysc_regs_i.gpsr_spi_mosi_i <= '0';
-  sysc_regs_i.gpsr_spi_miso_i <= spi_miso_i;
 
+  sysc_regs_i.gpsr_spi_sclk <= '0';
+  sysc_regs_i.gpsr_spi_ncs  <= '0';
+  sysc_regs_i.gpsr_spi_mosi <= '0';
+  sysc_regs_i.gpsr_spi_miso <= spi_miso_i;
 
-  sysc_regs_i.hwbld_date_i <= g_hwbld_date;
+  sysc_regs_i.hwbld <= g_hwbld_date;
 
   -------------------------------------
   -- DIAG to/from external modules
   -------------------------------------
   -- first, provide all the constants
-  sysc_regs_i.diag_info_id_i  <= std_logic_vector(to_unsigned(g_diag_id, 16));
-  sysc_regs_i.diag_info_ver_i <= std_logic_vector(to_unsigned(g_diag_ver, 16));
-  sysc_regs_i.diag_nw_ro_i  <= std_logic_vector(to_unsigned(g_diag_ro_size, 16));
-  sysc_regs_i.diag_nw_rw_i <= std_logic_vector(to_unsigned(g_diag_rw_size, 16));
+  sysc_regs_i.diag_info_id  <= std_logic_vector(to_unsigned(g_diag_id, 16));
+  sysc_regs_i.diag_info_ver <= std_logic_vector(to_unsigned(g_diag_ver, 16));
+  sysc_regs_i.diag_nw_ro <= std_logic_vector(to_unsigned(g_diag_ro_size, 16));
+  sysc_regs_i.diag_nw_rw <= std_logic_vector(to_unsigned(g_diag_rw_size, 16));
 
   diag_array_out <= diag_out_regs;
   -- r/w registers can be also read
@@ -281,69 +281,48 @@ begin
   begin
     if rising_edge(clk_sys_i) then
       if rst_n_i = '0' then
-        diag_adr <= (others=>'0');
         diag_dat <= (others=>'0');
       else
-        if sysc_regs_o.diag_cr_adr_load_o = '1' then
-          diag_adr <= unsigned(sysc_regs_o.diag_cr_adr_o);
-        end if;
-        if sysc_regs_o.diag_dat_load_o = '1' then
-          diag_dat <= sysc_regs_o.diag_dat_o;
+        if sysc_regs_o.diag_dat_wr = '1' then
+          diag_dat <= sysc_regs_o.diag_dat;
         end if;
       end if;
     end if;
   end process;
 
-  sysc_regs_i.diag_cr_adr_i    <= std_logic_vector(diag_adr);
   GEN_DIAG_NODAT: if g_diag_rw_size = 0 and g_diag_ro_size = 0 generate
-    sysc_regs_i.diag_dat_i <= (others=>'0');
+    sysc_regs_i.diag_dat <= (others=>'0');
   end generate;
   GEN_DIAG_DAT: if g_diag_rw_size /= 0 or g_diag_ro_size /= 0 generate
-    sysc_regs_i.diag_dat_i <= diag_in(to_integer(diag_adr));
+    sysc_regs_i.diag_dat <= diag_in(to_integer(unsigned(sysc_regs_o.diag_cr_adr)));
   end generate;
 
   -- Write request for each r/w register
-  GEN_DIAG_W: if g_diag_rw_size > 0 generate
-    GEN_LOOP: for I in 0 to g_diag_rw_size-1 generate
-
-      process(clk_sys_i)
-      begin
-        if rising_edge(clk_sys_i) then
-          if rst_n_i = '0' then
-            diag_out_regs(I) <= (others=>'0');
-          elsif sysc_regs_o.diag_cr_adr_load_o = '1' and sysc_regs_o.diag_cr_rw_o = '1' and
-            to_integer(unsigned(sysc_regs_o.diag_cr_adr_o)) = I then
-              diag_out_regs(I) <= diag_dat;
-          end if;
+  GEN_LOOP: for I in 0 to g_diag_rw_size-1 generate
+    process(clk_sys_i)
+    begin
+      if rising_edge(clk_sys_i) then
+        if rst_n_i = '0' then
+          diag_out_regs(I) <= (others=>'0');
+        elsif sysc_regs_o.diag_cr_wr = '1' and sysc_regs_o.diag_cr_rw = '1'
+          and to_integer(unsigned(sysc_regs_o.diag_cr_adr)) = I
+        then
+          diag_out_regs(I) <= diag_dat;
         end if;
-      end process;
-
-    end generate;
-  end generate;
-
-  GEN_NODIAG_W: if g_diag_rw_size = 0 generate
-    diag_array_out <= (others=>(others=>'0'));
+      end if;
+    end process;
   end generate;
 
   ----------------------------------------
   -- SYSCON
   ----------------------------------------
-  SYSCON : entity work.wrc_syscon_wb
+  inst_wrc_syscon_map: entity work.wrc_syscon_map
     port map (
-      rst_n_i    => rst_n_i,
-      clk_sys_i  => clk_sys_i,
-      wb_adr_i   => syscon_wb_i.adr(5 downto 2), -- shift address for word addressing
-      wb_dat_i   => syscon_wb_i.dat,
-      wb_dat_o   => syscon_wb_o.dat,
-      wb_cyc_i   => syscon_wb_i.cyc,
-      wb_sel_i   => syscon_wb_i.sel,
-      wb_stb_i   => syscon_wb_i.stb,
-      wb_we_i    => syscon_wb_i.we,
-      wb_ack_o   => syscon_wb_o.ack,
-      wb_err_o   => syscon_wb_o.err,
-      wb_rty_o   => syscon_wb_o.rty,
-      wb_stall_o => syscon_wb_o.stall,
-      regs_i     => sysc_regs_i,
-      regs_o     => sysc_regs_o);
-
+      rst_n_i => rst_n_i,
+      clk_i => clk_sys_i,
+      wb_i => syscon_wb_i,
+      wb_o => syscon_wb_o,
+      sysc_regs_i => sysc_regs_i,
+      sysc_regs_o => sysc_regs_o
+      );
 end struct;
