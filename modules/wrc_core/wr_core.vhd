9d2d7db03a8cd9f4d54b72818a1f20cddfd19bec
Alen Arias Vazquez <alen.arias.vazquez@cern.ch>
2026-01-29 13:41:37 +0100

    wr_core.vhd: replace g_rx_buffer_size for g_ep_rxbuf_size in the instance of xwr_core

--- modules/wrc_core/wr_core.vhd
+++ modules/wrc_core/wr_core.vhd
@@ -365,7 +365,7 @@ begin
       g_phys_uart_tx_fifo_size    => g_phys_uart_tx_fifo_size,
       g_phys_uart_rx_fifo_size    => g_phys_uart_rx_fifo_size,
       g_virtual_uart              => g_virtual_uart,
-      g_rx_buffer_size            => g_rx_buffer_size,
+      g_ep_rxbuf_size             => g_rx_buffer_size,
       g_tx_runt_padding           => g_tx_runt_padding,
       g_with_external_clock_input => g_with_external_clock_input,
       g_aux_clks                  => g_aux_clks,
