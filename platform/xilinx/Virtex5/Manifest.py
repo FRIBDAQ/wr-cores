866122dc8434737ff3981c3d047dea1f9589525e
Tristan Gingold <tristan.gingold@cern.ch>
2026-02-18 14:31:38 +0100

    Renaming: GTP-5 -> GTP
    According to ug196, this first generation of GTP has no generation name

--- platform/xilinx/Virtex5/Manifest.py
+++ platform/xilinx/Virtex5/Manifest.py
@@ -3,5 +3,5 @@
 ##
 ## SPDX-License-Identifier: LGPL-2.1-or-later
 ###############################################################################
-modules = {"local" : ["chipscope", "GTP-5"]}
+modules = {"local" : ["chipscope", "GTP"]}
 
